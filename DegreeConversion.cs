e73fe583042b56c710411b2a377fb60bc6cd14c6
BootCampBL <satyabhandari43@gmail.com>
2021-07-12 20:35:23 +0530

    [sathya] added- Degree Conversion

---
+++ DegreeConversion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramsForNUnitTesting
+{
+    class DegreeConversion
+    {
+		public static void Degree()
+		{
+			int Celcius;
+			Console.WriteLine("Enter the Temperature in Celcius");
+			Celcius = int.Parse(Console.ReadLine());
+			//Converting Celsius to Fahrenheit
+			int Fahrenheit = (Celcius * 9 / 5) + 32;
+			Console.WriteLine("After Converting: " + Fahrenheit);
+
+			Console.WriteLine("Enter the Farhenheit");
+			Celcius = int.Parse(Console.ReadLine());
+			Celcius = (Fahrenheit - 32) * 5 / 9;
+			Console.WriteLine("After Conveting: " + Celcius);
+
+		}
+
+	}
+}
+    
