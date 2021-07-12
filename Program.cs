d712d21ec7774c3591987fa5d2b626b45e0536bd
BootCampBL <satyabhandari43@gmail.com>
2021-07-12 20:30:54 +0530

    [sathya] added- program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProgramsForNUnitTesting
+{
+    class Program
+    {
+        public static void main(string[] args)
+        {
+            //VendingMachine.countCurrency();
+            //DaysOfWeek.days();
+            //DegreeConversion.Degree();
+            //MonthlyPayment.pay();
+            NewtonsMethod.square();
+            //DecimalToBinary.tobinary();
+
+        }
+    }
+}
+
